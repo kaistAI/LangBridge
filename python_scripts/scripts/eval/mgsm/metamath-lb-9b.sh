c3f02dbba3eab3dbd7577b33dae440bcd43bb0e4
Dongkeun Yoon <57797966+MattYoon@users.noreply.github.com>
2024-01-25 22:22:33 +0900

    fix typo

--- python_scripts/scripts/eval/mgsm/metamath-lb-9b.sh
+++ python_scripts/scripts/eval/mgsm/metamath-lb-9b.sh
@@ -3,7 +3,7 @@ export HF_DATASETS_CACHE=/mnt/sda/dongkeun/huggingface_datasets
 export CUDA_VISIBLE_DEVICES=3
 
 python eval_langbridge.py \
-  --checkpoint_path kaist-ai/metamath-langbrige_9b\
+  --checkpoint_path kaist-ai/metamath-langbrige-9b\
   --enc_tokenizer kaist-ai/langbridge_encoder_tokenizer \
   --tasks mgsm_en,mgsm_es,mgsm_fr,mgsm_de,mgsm_ru,mgsm_zh,mgsm_ja,mgsm_th,mgsm_sw,mgsm_bn,mgsm_te\
   --instruction_template metamath \
