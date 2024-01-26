686e828311c9cb39494d19ef3d6ce4c3f087a986
Dongkeun Yoon <57797966+MattYoon@users.noreply.github.com>
2024-01-26 20:04:29 +0900

    Update llemma7b.sh

--- python_scripts/scripts/eval/mgsm/llemma7b.sh
+++ python_scripts/scripts/eval/mgsm/llemma7b.sh
@@ -2,7 +2,7 @@ export TRANSFORMERS_CACHE=/mnt/sda/dongkeun/huggingface
 export HF_DATASETS_CACHE=/mnt/sda/dongkeun/huggingface_datasets
 export CUDA_VISIBLE_DEVICES=1
 
-python eval.py \
+python eval_baseline.py \
   --model hf-causal-experimental \
   --model_args dtype="bfloat16",pretrained=EleutherAI/llemma_7b \
   --tasks mgsm_en,mgsm_es,mgsm_fr,mgsm_de,mgsm_ru,mgsm_zh,mgsm_ja,mgsm_th,mgsm_sw,mgsm_bn,mgsm_te\
